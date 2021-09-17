ed1fbea8fb81419dacb23d6b98b78a9af2469417
Ja Young Cho <jayoung8966@gmail.com>
2021-09-17 20:03:53 +0200

    image update

--- css-sass/src/components/about.vue
+++ css-sass/src/components/about.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div>
+    <div class="picture"></div>
+    <h1>
+      Deine Herausforderung freundliche Frontend-Entwicklerin <br />
+      mit Designer Wurzeln <br />in Hannover
+    </h1>
+  </div>
 </template>
 
 <script>
@@ -7,3 +13,12 @@ export default {
   name: 'about',
 };
 </script>
+
+<style scoped>
+.picture {
+  background-image: url(../assets/mypic.jpeg);
+  width: 20vw;
+  height: 30vh;
+  padding: 2vw;
+}
+</style>
--- css-sass/src/views/About.vue
+++ css-sass/src/views/About.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="about">
-    <img src="@/assets/mypic.jpeg" alt="picture of me" class="picture" />
-    <h1>
-      Deine Herausforderung freundliche Frontend-Entwicklerin <br />
-      mit Designer Wurzeln <br />in Hannover
-    </h1>
-  </div>
+  <about></about>
 </template>
 
-<style scoped>
-.picture {
-  width: 30vw;
-  padding: 3vw;
-}
-</style>
+<script>
+import about from '@/components/about.vue';
+
+export default {
+  name: 'About',
+  components: {
+    about,
+  },
+};
+</script>
