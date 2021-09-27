aa05d0bdc6e36df7ed905f403fda796fceee1b63
Ja Young Cho <jayoung8966@gmail.com>
2021-09-27 15:49:01 +0200

    updating scss files

--- src/components/natour.vue
+++ src/components/natour.vue
@@ -27,55 +27,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../style/abstracts/functions';
+@import '../style/abstracts/variables';
+
 @import '../style/base/base';
 @import '../style/base/animations';
 @import '../style/base/typography';
 @import '../style/base/utility';
 
-$color-primary: #55c57a;
-$color-primary-light: #7ed56f;
-$color-primary-dark: #28b485;
-$color-grey-dark: #777;
-$color-white: #fff;
-$color-black: #000;
-
-html {
-  font-size: 62.5%;
-}
-body {
-  font-family: 'Lato', sans-serif;
-  font-weight: 400;
-  font-size: 1.6rem;
-  line-height: 1.7;
-  color: #777;
-  padding: 3rem;
-}
-
-.header {
-  height: 95vh;
-  background-image: linear-gradient(to right bottom, #7ed56fb4, #28b485),
-    url(../assets/img-natour/hero.jpg);
-  background-size: cover;
-  background-position: top;
-  clip-path: polygon(0 0, 100% 0, 100% 70%, 0 100%);
-  position: relative;
-
-  &__logobox {
-    position: absolute;
-    top: 3rem;
-    left: 3rem;
-  }
-  &__logo {
-    height: 3rem;
-  }
-  &__textBox {
-    position: absolute;
-    top: 40%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-  }
-}
+@import '../style/components/button';
 
 .headingPrimary {
   color: white;
@@ -99,86 +59,6 @@ body {
     letter-spacing: 2rem;
     animation-name: moveInRight;
     animation-duration: 2.3s;
-  }
-}
-
-@keyframes moveInLeft {
-  0% {
-    opacity: 0;
-    transform: translateX(-10rem);
-  }
-  80% {
-    transform: translateX(10px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateX(0);
-  }
-}
-
-@keyframes moveInRight {
-  0% {
-    opacity: 0;
-    transform: translateX(10rem);
-  }
-  80% {
-    transform: translateX(-1rem);
-  }
-  100% {
-    opacity: 1;
-    transform: translateX(0);
-  }
-}
-
-.btn {
-  &:link,
-  &:visited {
-    text-transform: uppercase;
-    text-decoration: none;
-    padding: 1.5rem 4rem;
-    display: inline-block;
-    border-radius: 10rem;
-    transition: all 0.2s;
-  }
-
-  &--white {
-    background-color: $color-white;
-    color: $color-grey-dark;
-
-    &::after {
-      background-color: $color-white;
-    }
-  }
-
-  &:hover {
-    transform: translateY(-3px);
-    box-shadow: 0 1rem 2rem rgba($color-black, 0.253);
-
-    &::after {
-      transform: scaleX(1.4) scaleY(1.6);
-      opacity: 0;
-    }
-  }
-  &:active {
-    transform: translateY(-1px);
-    box-shadow: 0 0.5rem 1rem rgba($color-black, 0.2);
-  }
-  &::after {
-    content: '';
-    display: inline-block;
-    height: 100%;
-    width: 100%;
-    border-radius: 10rem;
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-    transition: all, 0.4s;
-  }
-
-  &--animated {
-    animation: moveInBottom 0.5s ease-out 0.75s;
-    animation-fill-mode: backwards;
   }
 }
 </style>
